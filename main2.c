93d5244b305d9f79ab9c0f357f22df73a525305b
prairiekhun <139833857+prairiekhun@users.noreply.github.com>
2023-07-21 20:43:27 +0700

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PK(TUE)
+//
+//  Created by Prapatsorn Kaewsorndeetrikhun on 18/7/2566 BE.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("sky");
+    return 0;
+}
